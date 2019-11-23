15cf356bb9ce16237e2db51486fa0ea0d0b78e53
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2019-11-23 19:55:16 +0100

    Some improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {Router} from "@angular/router";
+import {MatSidenav} from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild(MatSidenav, {static: false}) sidenav: MatSidenav;
   title = 'original-case-client';
+
+  constructor(private _router: Router) {}
+
+  ngOnInit() {
+    this._router.events.subscribe(() => {
+      this.sidenav.close();
+    });
+  }
 
 }
